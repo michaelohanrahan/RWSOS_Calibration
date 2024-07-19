021cd54a814457c5f747e56e040f6cc693f6c095
Michael O'Hanrahan <104068149+michaelohanrahan@users.noreply.github.com>
2024-07-19 16:13:42 +0200

    building a validated network

--- meuse/data/1-external/assess_discharge_data.py
+++ meuse/data/1-external/assess_discharge_data.py
@@ -7,12 +7,9 @@ from argparse import ArgumentParser as AP
 import traceback
 import sys
 import numpy as np
-<<<<<<< HEAD:meuse/data/1-external/assess_discharge_data.py
-import geopandas as gpd
-=======
 import dask
->>>>>>> 13294849e6b5fc61d23333e82c83108544645b68:meuse/data/1-external/create_discharge_data_v2.py
 from shapely.geometry import Point
+import matplotlib.pyplot as plt
 
 def clip_nan(da):
     # Convert to pandas Series
@@ -36,33 +33,7 @@ def clip_nan(da):
     r_da = r_da.set_index(wflow_id='wflow_id', runs='runs', time='time')
     
     return r_da
-<<<<<<< HEAD:meuse/data/1-external/assess_discharge_data.py
-    
-'''
-EXAMPLE DATASET COMPATIBLE WITH ORIGINAL WORKFLOW
 
-xarray.Dataset
-Dimensions:
-    Q_gauges_obs:   67    
-    time:           105937
-
-Coordinates:
-    Q_gauges_obs    (Q_gauges_obs)  <U8             '12105900' ... '12089500'
-    time            (time)          datetime64[ns]  1986-10-01T06:00:00 ... 2023-01-...
-    
-Data variables:
-    Q               (time, Q_gauges_obs)  float64
-    
-Indexes:
-    Q_gauges_obs    PandasIndex
-    time            PandasIndex
-Attributes: (0)
-
-KEY DIFFERENCES TO EMPLOY ARE THE TWO PREVIOUS MODEL RUNS... Those will be HBV and FL1D in addition to Q
-'''
-=======
-
->>>>>>> 13294849e6b5fc61d23333e82c83108544645b68:meuse/data/1-external/create_discharge_data_v2.py
 def health_check(ds: xr.Dataset, health_check_path: str):
     valid_stations = []  # Step 1: Initialize an empty list
 
@@ -71,58 +42,83 @@ def health_check(ds: xr.Dataset, health_check_path: str):
         
     for id in ds.wflow_id.values:
         q_data = ds.sel(wflow_id=id).Q
-        nans = int(np.isnan(q_data).sum().compute())
         non_nan_indices = np.where(~np.isnan(q_data.compute()))[0]
         
-        if non_nan_indices.size > 0:
-            min_nonnan_date = q_data.time[non_nan_indices[0]].values
-            max_nonnan_date = q_data.time[non_nan_indices[-1]].values
-            valid_count = len(non_nan_indices)
-            time_diffs = np.diff(q_data.time[non_nan_indices].values.astype('datetime64[h]'))
+        # Filter out zeros from non-NaN indices
+        non_zero_non_nan_indices = non_nan_indices[q_data[non_nan_indices] != 0]
+        
+        if non_zero_non_nan_indices.size > 0:
+            min_nonnan_date = q_data.time[non_zero_non_nan_indices[0]].values
+            min_doi = pd.to_datetime(np.datetime64('2005-01-01T00:00:00.000000000')).strftime('%Y-%m-%d %H:%M:%S')
+            max_nonnan_date = q_data.time[non_zero_non_nan_indices[-1]].values
+            nans = int(np.isnan(q_data.sel(time=slice(min_doi, max_nonnan_date))).sum().compute())
+            valid_count = len(non_zero_non_nan_indices)
+            time_diffs = np.diff(q_data.time[non_zero_non_nan_indices].values.astype('datetime64[h]'))
             most_common_diff = pd.Series(time_diffs).mode()[0]
             frequency = f"{most_common_diff / np.timedelta64(1, 'h')} hours"
+            total_len = int(len(pd.date_range(min_doi, max_nonnan_date, freq='H')))
         else:
-            min_nonnan_date = 'all NaN'
-            max_nonnan_date = 'all NaN'
+            min_nonnan_date = 'all NaN or zero'
+            max_nonnan_date = 'all NaN or zero'
             valid_count = 0
             frequency = 'N/A'
+            total_len = 0
+            
         
         zeros = int((q_data == 0).sum().compute())
         
         try:
             station_name = ds.sel(wflow_id=id).station_name.data.compute()
+            if ',' in station_name:
+                station_name = station_name.split(',')[0]
         except:
             try:
                 station_name = ds.sel(wflow_id=id)['Libellé'].data.compute()
             except:
-                station_name = 'name not found'
+                station_name = None
+        
+        plotted = False
+        if not os.path.exists(f'observed_discharge/visualisation/{station_name}.png') and station_name not in ['None', None] and os.path.exists(r"P:\11209265-grade2023\wflow\RWSOS_Calibration\meuse\data\2-interim\QGIS\distance_filtered_10pct_keephighzeros_hourly_gauges.geojson"):
+            plot(ds.sel(wflow_id=id).Q, station_name)
+            plotted = True
         
         nan_proportion = nans / valid_count if valid_count > 0 else 0.
         
-        if nan_proportion <= 0.25 and non_nan_indices.size > 0:  # Step 2: Check if proportion 
-            valid_stations.append({
-                'id': int(id),
-                'name': str(station_name),
-                'invalid_proportion': float(nan_proportion),
-                'valid_count': int(valid_count),
-                'min_date': str(min_nonnan_date),
-                'max_date': str(max_nonnan_date),
-                'frequency': str(frequency),
-                'zeros': int(zeros),
-                'x': float(ds.sel(wflow_id=id).x.values),
-                'y': float(ds.sel(wflow_id=id).y.values)
-            })
+        # if nan_proportion <= 0.25 and non_nan_indices.size > 0:  # Step 2: Check if proportion 
+        valid_stations.append({
+            'id': int(id),
+            'name': str(station_name),
+            'invalid_proportion_2005-max': float(nan_proportion),
+            'total_valid_2005-max': int(valid_count),
+            'total_len_2005-max': total_len,
+            'min_date': str(min_nonnan_date),
+            'min_doi': str(min_doi),
+            'max_date': str(max_nonnan_date),
+            'frequency': str(frequency),
+            'zeros': int(zeros),
+            'x': float(ds.sel(wflow_id=id).x.values),
+            'y': float(ds.sel(wflow_id=id).y.values),
+            'plotted':plotted
+        })
         
-            with open(health_check_path, 'a') as f:
-                f.write(f'{"."*50}\nWF_id: {id}, len valid: {valid_count}, proportion NaN: {nan_proportion:.3f}, frequency: {frequency}, Station: {station_name}, \n\
-                        first valid date: {min_nonnan_date}, last valid date: {max_nonnan_date}, \n\
-                        n zeros: {zeros}\n {"."*50}\n')
+        with open(health_check_path, 'a') as f:
+            f.write(f'{"."*50}\nWF_id: {id}, len valid: {valid_count}, proportion NaN: {nan_proportion:.3f}, frequency: {frequency}, Station: {station_name}, \n\
+                    first valid date: {min_nonnan_date}, last valid date: {max_nonnan_date}, \n\
+                    n zeros: {zeros}\n {"."*50}\n')
     return valid_stations
+
+def plot(series, name):
+    fig, axs = plt.subplots(3, 1, figsize=(15, 10))
+    plt.suptitle(name)
+    series.plot(ax=axs[0])
     
-<<<<<<< HEAD:meuse/data/1-external/assess_discharge_data.py
-  
-=======
->>>>>>> 13294849e6b5fc61d23333e82c83108544645b68:meuse/data/1-external/create_discharge_data_v2.py
+    series.sel(time=slice('2005-01-01', '2005-12-31')).plot(ax=axs[1])
+    axs[1].set_title('2005')
+    series.sel(time=slice('2011-01-01', '2011-12-31')).plot(ax=axs[2])
+    axs[2].set_title('2011')
+    os.makedirs('observed_discharge/visualisation', exist_ok=True)
+    plt.savefig(f'observed_discharge/visualisation/{name}.png', dpi=300)
+
 def get_dc_data(ls:list, model:str, cwd:str, plat:str, freq:str='H'):
     '''
     The main function is passed the argument of a list of wflow ids to combine into a subcatchment set
@@ -152,52 +148,33 @@ def get_dc_data(ls:list, model:str, cwd:str, plat:str, freq:str='H'):
     sources = [f for f in datacatalog.keys if flong in f and 'stats' not in f]
     print(f'{flong} keys: {sources}')
     
-    health_check_path = os.path.join(cwd, f'{flong}_health_check.txt')
+    health_check_path = os.path.join(cwd, f'observed_discharge/{flong}_health_check.txt')
     
     if os.path.exists(health_check_path):
         os.remove(health_check_path)
-<<<<<<< HEAD:meuse/data/1-external/assess_discharge_data.py
-        
-    all_stations = []
-=======
     
     all_stations = []  # Initialize an empty list to store all valid stations
     
->>>>>>> 13294849e6b5fc61d23333e82c83108544645b68:meuse/data/1-external/create_discharge_data_v2.py
     for key in sources:
         ds = datacatalog.get_dataset(key)
         src = datacatalog.get_source(key)
-        with open(health_check_path, 'a') as f:
-            f.write(f'\n {"="*100} \nWorking on {key}\n')
-            f.write(f'dataset: {ds}\n')
-            
         print(f'\n {"*"*20} \nWorking on {key}')
         print(f'dataset: {ds}')
         valid_stations = health_check(ds, health_check_path)  # Updated variable name here
-<<<<<<< HEAD:meuse/data/1-external/assess_discharge_data.py
-=======
         #create geojson 
->>>>>>> 13294849e6b5fc61d23333e82c83108544645b68:meuse/data/1-external/create_discharge_data_v2.py
         all_stations.extend(valid_stations)
         print(f'Finished {key}\n {"*"*20}')
-    # Step 3: Convert the list of dictionaries into a DataFrame
-    df_valid_stations = pd.DataFrame(all_stations)
-    print(df_valid_stations)
-    # Step 4: Convert DataFrame to GeoDataFrame and save as GeoJSON
-    gdf = gpd.GeoDataFrame(df_valid_stations, geometry=[Point(xy) for xy in zip(df_valid_stations['x'], df_valid_stations['y'])])
-    gdf.to_file("valid_stations.geojson", driver='GeoJSON')
     
     # Step 3: Convert the list of dictionaries into a DataFrame
     df_valid_stations = pd.DataFrame(all_stations)
     print(df_valid_stations)
     # Step 4: Convert DataFrame to GeoDataFrame and save as GeoJSON
     gdf = gpd.GeoDataFrame(df_valid_stations, geometry=[Point(xy) for xy in zip(df_valid_stations['x'], df_valid_stations['y'])])
-    gdf.to_file(f"{flong}_valid_stations.geojson", driver='GeoJSON')
+    gdf.to_file(f"observed_discharge/{flong}_stations.geojson", driver='GeoJSON')
     
   
 if __name__ == '__main__':
     parser = AP()
-    parser.add_argument('--wflow_ids', type=list, default=[9])
     parser.add_argument('--model', type=str, default='meuse')
     parser.add_argument('--cwd', type=str, default=r"p:\11209265-grade2023\wflow\RWSOS_Calibration\meuse\data\1-external")
     parser.add_argument('--freq', type=str, default='H')
@@ -207,8 +184,8 @@ if __name__ == '__main__':
         plat=''
     else:
         plat='_linux'
+        
     try:
-        print('')
         get_dc_data(args.wflow_ids, args.model, args.cwd, plat, args.freq)
     except Exception as e:
         print(f'Error: {e}')
