8f1f674afa7a019e7943fa65ba2dec070ab63974
JingDeng92 <162152251+JingDeng92@users.noreply.github.com>
2024-08-07 17:00:25 +0200

    add for upstream gauge: random select and modify staticmap

--- meuse/src/calib/set_calib_params.py
+++ meuse/src/calib/set_calib_params.py
@@ -1,3 +1,6 @@
+#%% TODO: 1) how to access best_10params: list[Path | str]. 2) how to save the random select paramset results
+
+#%%
 from pathlib import Path
 import pandas as pd
 import geopandas as gpd
@@ -10,13 +13,27 @@ import os
 import random
 import numpy as np
 
+def select_random_paramset(df, gauge):
+    """
+    df: csv that contains the best_10params
+    gauge: the gauge id
+    """
+    row = df[df.index == gauge]
+    if row.empty:
+        raise ValueError("Gauge not found in DataFrame")
+    random_column = random.choice(row.columns)
+    random_paramset = row[random_column].apply(eval).values[0]
+
+    return random_column, random_paramset
+
 
 def main(
     l,
-    p: list[Path | str],  # a list of 10 staticmaps paths from the previous level
+    p: Path | str,
     params: dict,
     params_lname: tuple | list,
     params_method: tuple | list,
+    best_10params: list[Path | str], #TODO: a list of best_10params.csv from previous levels
     level: str,
     graph: dict,
     sub_catch: Path | str,
@@ -28,55 +45,59 @@ def main(
     Load the dataset (staticmaps) then update params for the desired gauge_ids
     the paramslname should be the same as the dataset key
     """
-    gauge_ids = graph[level]["elements"]
-    
-    # # Load the current main dataset
-    # ds = xr.open_dataset(p)
-    # l.info(f"Loading dataset from {p}")
-    
-    # Make integers of the gauge_ids
-    gauge_int = [
-        int(item) for item in gauge_ids
-    ]
-    l.info(f"Updating the following gauge_ids: {gauge_int}")
+    # Load original staticmaps
+    ds = xr.open_dataset(p)
 
     # Load the geometries
-    # vds
     vds = gpd.read_file(sub_catch)
+    vds = vds.astype({"value": int})
     
-    # Get the relevant sub catchments
-    #vds_d is now a geodataframe
-    vds_d = vds_d.astype({"value": int})
-    #vds_d is now a geodataframe with only the gauges of interest
-    vds_d = vds_d[vds_d.value.isin(gauge_int)]
-    
-    # # Create the data mask
-    # mask = ds.raster.geometry_mask(vds_d)
-
-    # Loop through the parameters
+    # Set param for current level gauges
+    gauge_ids = graph[level]["elements"]
+    gauge_int = [int(item) for item in gauge_ids]
+    l.info(f"Updating the following current level gauges: {gauge_int}")
+    vds_current = vds[vds.value.isin(gauge_int)]
+    mask = ds.raster.geometry_mask(vds_current)
     for idx, (key, value) in enumerate(params.items()):
-        # randomly select a staticmap
-        selected_staticmap_path = random.choice(p)
-        ds = xr.open_dataset(selected_staticmap_path)
-        l.info(f"Loading dataset from {selected_staticmap_path}")
-        
-        # Create the mask based on the geometries
-        mask = ds.raster.geometry_mask(vds_d)
-        
-        #The key is the var id
-        vds_d[key] = value
+        vds[key] = value   # what is this line used for?
         da = ds[params_lname[idx]]
         if params_method[idx] == "mult":
             da.values[mask] *= value
         elif params_method[idx] == "set":
             da.values[mask] = value
         elif params_method[idx] == "add":
-            da.values[mask] += value          
+            da.values[mask] += value
         ds[params_lname[idx]] = da
+    
+    # Set param for upstream gauges
+    upgauge_ids = graph[level]["deps"]
+    upgauge_int = [int(item) for item in upgauge_ids]
+    l.info(f"Updating the following upstream gauges: {upgauge_int}")
+    random_sel_results = []
+    # for each upstream gauge, set random param from best_10params.csv
+    for upgauge in upgauge_int:
+        vds_upgauge = vds[vds.value == upgauge]
+        mask_up = ds.raster.geometry_mask(vds_upgauge)
         
-        # Save the modified staticmap, TODO: save one or list of all modified staticmaps?
-        ds.to_netcdf(out)
-        l.info(f"Writing dataset to {out}")
+        #TODO: find the csv that contain this upgauge from best_10params => lets call it csv for now
+        # select one from top1 to top10 for this upgauge
+        random_column, random_paramset = select_random_paramset(csv, upgauge)
+        random_sel_results.append({'upgauges': upgauge, 'selected_column': random_column, 'selected_paramset': random_paramset})
+        # modify staticmap based on the random paramset
+        for idx, value in enumerate(random_paramset.values()):
+            da = ds[params_lname[idx]]
+            if params_method[idx] == "mult":
+                da.values[mask_up] *= value
+            elif params_method[idx] == "set":
+                da.values[mask_up] = value
+            elif params_method[idx] == "add":
+                da.values[mask_up] += value          
+            ds[params_lname[idx]] = da
+            
+    df_random_sel_results = pd.DataFrame(random_sel_results)  #TODO: how to save this?
+    
+    ds.to_netcdf(out)
+    l.info(f"Writing dataset to {out}")
 
     #make sure the lakes h-q rating relation is also copied
     for lin, lout in zip(lakes_in, lakes_out):
@@ -88,21 +109,28 @@ def main(
 if __name__ == "__main__":
     
     work_dir = Path(r'c:\Users\deng_jg\work\05wflowRWS\UNREAL_TEST_DATA')
+    csv = pd.read_csv(work_dir / 'best_10params.csv', index_col='gauges')
+    csv.index=csv.index.astype(int)
+    
+    p = work_dir / 'staticmaps/staticmaps.nc'
     
     import pickle as pk
     with open(work_dir/'create_set_params.pkl', 'rb') as f:
         dict = pk.load(f)
-    lnames = dict['lnames']
-    methods = dict['methods']
+    params_lname = dict['lnames']
+    params_method = dict['methods']
     df = dict['ds']
     
     from snakemake.utils import Paramspace
     paramspace = Paramspace(df)
     params = paramspace.instance
     
+    level = 'level5'
+    import json
+    graph = json.load(open(Path(work_dir / 'Hall_levels_graph.json')))
+    sub_catch = work_dir / 'subcatch_Hall.geojson'
     
-    
-    
+        
     
     
     l=setup_logging('data/0-log', '03-set_calib_params.log')
