7431b65dffd94ef4dac82596af7d06b8d066513c
JoostBuitink <joost.buitink@deltares.nl>
2024-09-16 08:55:56 +0200

    fix bug with function arguments

--- meuse_random/src/calib/combine_evaluated.py
+++ meuse_random/src/calib/combine_evaluated.py
@@ -6,10 +6,10 @@ import pandas as pd
 def read_results_to_dataframe(results_file: Path) -> pd.DataFrame:
     """
     Read the results txt file into a DataFrame.
-    
+
     Args:
     results_file (Path): Path to the results txt file.
-    
+
     Returns:
     pd.DataFrame: DataFrame with gauge measurements as columns and parameter sets as index.
     """
@@ -25,7 +25,7 @@ def read_results_to_dataframe(results_file: Path) -> pd.DataFrame:
                 param_set = parts[0] + '}'  # Add the closing brace back
                 values = parts[1].strip().split(',')
                 data.append((param_set, values))
-    
+
     # Extract parameter sets and values
     param_sets = [row[0] for row in data]
     values = []
@@ -36,14 +36,14 @@ def read_results_to_dataframe(results_file: Path) -> pd.DataFrame:
                 row_values.append(float(val))
             except ValueError:
                 row_values.append(float('nan'))  # Convert empty strings or invalid floats to NaN
-        #remove the nan values 
+        #remove the nan values
         row_values = [val for val in row_values if not pd.isna(val)]
         values.append(row_values)
-    
+
     # Create DataFrame
     df = pd.DataFrame(values, columns=header[1:], index=param_sets)
     df.index.name = 'params'
-    
+
     return df
 
 def main(
@@ -55,7 +55,7 @@ def main(
 ):
     df = read_results_to_dataframe(results_file)
     # ic(df)
-    
+
     best_params = []
     for gauge in df.columns:
         #gauge is a float column
@@ -68,8 +68,8 @@ def main(
             param_dict =valid_ranked.index[idx]
             best_for_gauge[f'Top_{idx+1}'] = param_dict
         best_params.append(best_for_gauge)
-    
-    
+
+
     #TODO: Add the level column to best_params csv
     # Convert the results to a DataFrame and save as CSV
     if level == 0:
@@ -79,14 +79,14 @@ def main(
         inter_df = pd.DataFrame(best_params)
         inter_df['level'] = level
         prev_df = pd.read_csv(Path(out).parent.parent / f"level{level-1}" / "best_params.csv")
-        # append the new rows, there will be an extension of gauges and level, 
+        # append the new rows, there will be an extension of gauges and level,
         # but the columns will be the same Top_1 ... Top_n
         final_df = pd.concat([prev_df, inter_df], ignore_index=True)
         # this way the next phase has access to the best 10 of all levels
-    
+
     # ic(final_df)
     final_df.to_csv(out, index=False)
-    
+
     # Create an 'eval.done' file to indicate completion
     with open(Path(out).parent / "level.done", "w") as f:
         f.write("")
@@ -98,18 +98,19 @@ if __name__ == "__main__":
     try:
         if 'snakemake' in globals():
             main(
-                l,
-                snakemake.input.results_file,
-                snakemake.output.best_params,
-                snakemake.params.level,
+                l=l,
+                results_file=snakemake.input.results_file,
+                out=snakemake.output.best_params,
+                level=snakemake.params.level,
             )
         else:
             base_dir = r"/p/11209265-grade2023/wflow/RWSOS_Calibration/meuse_random/data/2-interim/calib_data/level0"
             l.info(f"Base dir exists: {Path(base_dir).exists()}\n{base_dir}")
-            
+
             results_file = Path(base_dir) / "results_level0.txt"
-            main(l,
-                results_file,
+            main(
+                l=l,
+                results_file=results_file,
                 out="/p/11209265-grade2023/wflow/RWSOS_Calibration/meuse_random/data/2-interim/calib_data/level0/best_params.csv",
                 level="level0",
             )
